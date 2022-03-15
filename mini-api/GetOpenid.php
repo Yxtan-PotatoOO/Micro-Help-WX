e34be982792e488761907d6f2c1cc5562fe2155b
ling_thismoment@sina.cn <ling_thismoment@sina.cn>
2022-03-15 11:11:01 +0800

    Micro-Help-WX

--- mini-api/GetOpenid.php
+++ mini-api/GetOpenid.php
@@ -1,7 +1,7 @@
 <?php
 $code = $_GET["code"];
-$appid = "wx528329915b1d3db7";
-$secret = "797588b4edccc53ab3df4af9c44e1f2f";
+$appid = ""; // 自己的微信
+$secret = ""; // 自己的微信
 $api = 'https://api.weixin.qq.com/sns/jscode2session?appid=' . $appid . '&secret=' . $secret . '&js_code=' . $code . '&grant_type=authorization_code';
 
 $info = file_get_contents($api);
--- mini-api/connect.php
+++ mini-api/connect.php
@@ -1,8 +1,8 @@
 <?php
 $host_name = "localhost";
-$db_usrname = "sql_127_0_0_1";
-$db_pwd = "mRXZy5R5PfeNz6DB";
-$db_name = "sql_127_0_0_1";
+$db_usrname = ""; // 数据库用户名
+$db_pwd = ""; // 数据库密码
+$db_name = ""; // 数据库名
 
 $conn = mysqli_connect($host_name, $db_usrname, $db_pwd, $db_name)or trigger_error(mysqli_error(),E_USER_ERROR);
 
